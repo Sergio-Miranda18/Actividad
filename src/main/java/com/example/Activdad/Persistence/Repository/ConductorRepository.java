15c184bc9693cfa3fafd3655a0b555cee69008c9
Sergio Miranda <sm1723320@gmail.com>
2024-11-29 21:28:13 -0500

    act

--- src/main/java/com/example/Activdad/Persistence/Repository/ConductorRepository.java
+++ src/main/java/com/example/Activdad/Persistence/Repository/ConductorRepository.java
@@ -3,9 +3,10 @@ package com.example.Activdad.Persistence.Repository;
 
 import com.example.Activdad.Persistence.entity.Conductor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface ConductorRepository extends JpaRepository<Conductor, Long> {
 
 }
